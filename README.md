498666284d83cb1cb86858d49cdbc6984582d005
behnamhamidi1991 <eslp91@hotmail.com>
2024-07-18 15:08:18 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### What is the command to create a project
+- composer create-project laravel/laravel `project-name`
+
+
+### What is the command to start a project
+- php artisan serve
+
+
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return ('welcome');
+});
+
+Route::get('/blog', function() {
+    return "This is the blog page";
+})->name('blog');
+
+Route::get('/courses', function() {
+    return redirect('/blog')->name('blog-redirect');
+});
+
+Route::get('/blog/{id}', function ($id) {
+    return 'This is a post with the id of ' . $id . '!';  
+})->name('blog-single-post');
+
+
+// GET
+// POST
+// PUT
+// DELETE
